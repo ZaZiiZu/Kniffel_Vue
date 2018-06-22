43006055342b3e5ad32186a9342d0ca2145f8b87
Schulung1 <viktor4570@web.de>
2018-06-22 14:52:41 +0200

    added some tests

--- kniffelvue/src/components/TransfMatrices.vue
+++ kniffelvue/src/components/TransfMatrices.vue
@@ -39,12 +39,11 @@
 </template>
 
 <script>
-
 const math = require('mathjs')
 
 export default {
-  name: 'tests',
-  data () {
+  name: 'TransfMatrices',
+  data() {
     return {
       settings: {
         alternatives: 2,
@@ -56,20 +55,20 @@ export default {
 
     }
   },
-  watch: {
-  },
+  watch: {},
   computed: {
 
   },
   methods: {
-    get_transM () {
-      const alternatives = this.settings.alternatives
-      const size = this.settings.rolls
+    get_transM(alts_override, size_override) {
+      const alternatives = alts_override || this.settings.alternatives
+      const size = size_override || this.settings.rolls
       this.normMatrix = this.get_normMatrix(alternatives, size)
       this.corrMatrix = this.get_corrMatrix(alternatives, size, this.normMatrix)
       this.transfMatrix = this.normMatrix
+      return this.transfMatrix
     },
-    get_normMatrix (alternatives, size) {
+    get_normMatrix(alternatives, size) {
       const sheet = {}
       const cellDef = {
         value: 0,
@@ -85,12 +84,12 @@ export default {
       }
       const chanceOnly = 1 / alternatives
       const chanceExcept = (alternatives - 1) / alternatives
-      // console.log('chances: ', chanceOnly, chanceExcept)
+      // // console.log('chances: ', chanceOnly, chanceExcept)
       for (let fixed = size; fixed >= 1; fixed--) {
         sheet[fixed] = {}
         for (let need = size; need >= 1; need--) {
-          // console.log('------------------')
-          // console.log('id: ', fixed, need)
+          // // console.log('------------------')
+          // // console.log('id: ', fixed, need)
           let lowerRight = null
           let lowerLeft = null
           const cell = JSON.parse(JSON.stringify(cellDef))
@@ -100,12 +99,12 @@ export default {
           if (sheet && sheet[fixed + 1] && sheet[fixed + 1][need]) {
             lowerLeft = math.clone(sheet[fixed + 1][need])
           } else lowerLeft = math.clone(cellDef)
-          // console.log('lower right',
+          // // console.log('lower right',
           //   [fixed + 1] + " " + [need + 1] + " = " + JSON.stringify(lowerRight))
-          // console.log('lower left: ',
+          // // console.log('lower left: ',
           //   [fixed + 1] + " " + [need] + " = " + JSON.stringify(lowerLeft))
           cell.normOdds = (fixed === size) && (need === size) ? 1
-            : lowerLeft.normOdds * chanceOnly +
+              : lowerLeft.normOdds * chanceOnly +
               lowerRight.normOdds * chanceExcept
           cell.id = [fixed, need]
           // cell.value = math.combinations(size - fixed, need) || 0
@@ -114,7 +113,7 @@ export default {
       }
       return sheet
     },
-    get_corrMatrix (alternatives, size, normMatrix_input) {
+    get_corrMatrix(alternatives, size, normMatrix_input) {
       const normMatrix = JSON.parse(JSON.stringify(normMatrix_input))
       let sheet = {}
       const cellDef = {
@@ -150,23 +149,38 @@ export default {
 
           cell.flags = this.get_flags(normMatrix, sheet, [fixed, need], cell)
 
-          // console.log('looooop: ', fixed, need)
+          // // console.log('looooop: ', fixed, need)
           const asdfasdf = Object.keys(sheet[fixed][need].flags)
-          // console.log('asdfasdf: ', asdfasdf, cell.flags)
+          // // console.log('asdfasdf: ', asdfasdf, cell.flags)
           const filtered = asdfasdf.filter(x => sheet[fixed][need].flags[x] == true)
-          // console.log('filtered: ', filtered)
+          // // console.log('filtered: ', filtered)
           // if (filtered[0] === "negateFlag") {
           //   cell.corrOdds = this.sumif_row(Object.values(sheet[fixed]), filtered[0])
           // }
           switch (filtered[0]) {
-            case 'existsNot': cell.corrOdds = 0; break
-            case 'tooBig': cell.corrOdds = 0; break
-            case 'noChanceFlag': cell.corrOdds = (-1) * normMatrix[fixed][need].normOdds; break
-            case 'negateFlag': cell.corrOdds = this.negate(Object.values(sheet[fixed]), need); break
-            case 'evenOutFlag': cell.corrOdds = this.even_out(Object.values(normMatrix[fixed]), Object.values(sheet[fixed]), need); break
-            case 'fixOdds': cell.corrOdds = this.corrections(alternatives, fixed, need, size); break
-            case 'default': cell.corrOdds = 0; break
-            default: cell.corrOdds = 0
+            case 'existsNot':
+              cell.corrOdds = 0;
+              break
+            case 'tooBig':
+              cell.corrOdds = 0;
+              break
+            case 'noChanceFlag':
+              cell.corrOdds = (-1) * normMatrix[fixed][need].normOdds;
+              break
+            case 'negateFlag':
+              cell.corrOdds = this.negate(Object.values(sheet[fixed]), need);
+              break
+            case 'evenOutFlag':
+              cell.corrOdds = this.even_out(Object.values(normMatrix[fixed]), Object.values(sheet[fixed]), need);
+              break
+            case 'fixOdds':
+              cell.corrOdds = this.corrections(alternatives, fixed, need, size);
+              break
+            case 'default':
+              cell.corrOdds = 0;
+              break
+            default:
+              cell.corrOdds = 0
           }
           sheet = Object.assign({}, sheet)
           this.corrMatrix = sheet
@@ -176,13 +190,13 @@ export default {
       this.transfMatrix = sheet
       return sheet
     },
-    get_flags (normMatrix, sheet, idArray, cell) {
+    get_flags(normMatrix, sheet, idArray, cell) {
       const flags = JSON.parse(JSON.stringify(cell.flags))
       const fixed = idArray[0]
       const need = idArray[1]
       const size = Object.keys(normMatrix[fixed]).length
       const alternatives = this.settings.alternatives
-      // console.log('setting flags, fixed-need-size: ', fixed, need, size)
+      // // console.log('setting flags, fixed-need-size: ', fixed, need, size)
       if (normMatrix[fixed][need].normOdds <= 0) {
         flags.existsNot = true
         return flags
@@ -223,52 +237,53 @@ export default {
       flags.default = true
       return flags
     },
-    negate (row_array, trim_start) {
+    negate(row_array, trim_start) {
       let row = JSON.parse(JSON.stringify(row_array))
       row = row.filter(el => el.id[1] > trim_start)
-      console.log('row: ', row)
+      // console.log('row: ', row)
       const sum = this.sumArrayKey(row, 'corrOdds')
-      console.log('row_sum: ', sum)
+      // console.log('row_sum: ', sum)
       return (-1) * sum
     },
-    even_out (row_norm_array, row_corr_array, trim_start) {
+    even_out(row_norm_array, row_corr_array, trim_start) {
       let rowN = JSON.parse(JSON.stringify(row_norm_array))
       let rowC = JSON.parse(JSON.stringify(row_corr_array))
-      console.log('rows: ', rowN, rowC)
+      // console.log('rows: ', rowN, rowC)
       rowN = rowN.filter(el => el.id[1] > trim_start - 1)
       rowC = rowC.filter(el => el.id[1] > trim_start)
-      console.log('rows: ', rowN, rowC)
+      // console.log('rows: ', rowN, rowC)
       const sumN = this.sumArrayKey(rowN, 'normOdds')
       const sumC = this.sumArrayKey(rowC, 'corrOdds')
-      console.log('row_sums: ', sumN, sumC)
+      // console.log('row_sums: ', sumN, sumC)
       const result = 1 - sumN - sumC
       return result
     },
-    corrections (alt_i, fixed_i, need_i, size_i) {
+    corrections(alt_i, fixed_i, need_i, size_i) {
       const alts = alt_i
       const fixed = fixed_i
       const need = need_i
       const size = size_i
       const combinations = math.combinations(size - fixed, need)
       const factorX = math.min(math.floor((size - fixed) / need), alts - 1)
-      const correction = (alts - 1) / alts * (1 / alts) ** (need - 1) * ((alts - factorX) / alts) ** (size - fixed - need) * (combinations - factorX + 1)
+      const correction = (alts - 1) / alts * (1 / alts) ** (need - 1) * ((alts - factorX) / alts) ** (size - fixed -
+          need) * (combinations - factorX + 1)
       return correction
     },
-    sumArrayKey (array_of_objects, key_string, negative_include) {
+    sumArrayKey(array_of_objects, key_string, negative_include) {
       const choice = key_string || 'value'
       let row = JSON.parse(JSON.stringify(array_of_objects))
       if (!negative_include) {
         row = row.filter(el => el[choice] > 0)
       }
       let sum = 0
-      console.log('row for key: ', row, key_string)
+      // console.log('row for key: ', row, key_string)
       for (const el in row) {
-        console.log('element', el)
+        // console.log('element', el)
         if (row[el][choice] > 0) {
           sum += parseFloat(row[el][choice])
         }
       }
-      console.log('row_sum: ', sum)
+      // console.log('row_sum: ', sum)
       return sum
     }
   }
--- kniffelvue/tests/unit/TransfMatrices.spec.js
+++ kniffelvue/tests/unit/TransfMatrices.spec.js
@@ -1,35 +1,109 @@
-import transfMatrices from '../../components/TransfMatrices.vue'
+import TransfMatrices from '@/components/TransfMatrices'
 import {
-  createLocalVue,
-  shallow,
   mount
-} from 'vue-test-utils'
+} from '@vue/test-utils'
 
 describe('TransfMatrices_test', () => {
-  it('testNumeroUno', () => {
-    // Now mount the component and you have the wrapper
-    const wrapper = mount(transfMatrices, {
-      // propsData: { msg }
-    })
-    it('alternatives is a number', () => {
-      expect(wrapper.vm.settings.alternatives)
-        .toMatch(/[\d]/)
-    })
-    it('alternatives is >0', () => {
-      expect(wrapper.vm.settings.alternatives)
-        .toBeGreaterThen(0)
-    })
+  // Now mount the component and you have the wrapper
+  const wrapper = mount(TransfMatrices)
+  // console.log(wrapper.vm)
+  describe('constants exist', () => {
 
-    // it's also easy to check for the existence of elements
-    // it('has a button', () => {
-    //   expect(wrapper.contains('button')).toMatch(/[\d]/)
-    // })
-
-    // it('button should increment the count', () => {
-    //   expect(wrapper.vm.count).toBe(0)
-    //   const button = wrapper.find('button')
-    //   button.trigger('click')
-    //   expect(wrapper.vm.count).toBe(1)
-    // })
+    test('settings exist', () => {
+      expect(wrapper.vm.settings).toBeTruthy()
+    })
+    test('alternatives exist', () => {
+      expect(wrapper.vm.settings.alternatives).toBeTruthy()
+    })
   })
+  describe('constants are valid', () => {
+    test('alternatives is a number', () => {
+      expect(wrapper.vm.settings.alternatives).toBeNumber()
+    })
+    test('alternatives is >0', () => {
+      expect(wrapper.vm.settings.alternatives).toBeGreaterThan(0)
+    })
+
+  })
+  const alts_start = 2
+  const alts_end = 5
+  const rolls_start = 3
+  const rolls_end = 9
+  for (let i = alts_start; i <= alts_end; i++) {
+    for (let j = rolls_start; j <= rolls_end; j++) {
+      console.log('test for: ', i, j)
+      describe(`matrix for ${i} alts and ${j} size seems valid`, () => {
+        let result = wrapper.vm.get_transM(i, j)
+        test('matrix exists', () => {
+          expect(result).toBeTruthy()
+        })
+        console.log('test for: ', i, j)
+        let passLength = Object.keys(result).length == j
+
+        test('length seems fine', () => {
+          expect(passLength).toBeTruthy()
+        })
+
+        let passDepth = Object.keys(result[Object.keys(result)[0]]).length == j
+        test('depth also seems fine', () => {
+          expect(passDepth).toBeTruthy()
+        })
+        test('rows have sums of one', () => {
+          let objectKeySum = wrapper.vm.sumArrayKey(Object.values(result[1]), 'normOdds')
+          expect(objectKeySum).toBeCloseTo(1)
+        })
+      })
+    }
+  }
+  console.log('total tests should be: ', (alts_end - alts_start + 1) * (rolls_end - rolls_start + 1) * 4)
+
+  it.skip('has a button', () => {
+    expect(wrapper.contains('button')).toMatch(/[\d]/)
+  })
+  it.skip('button should increment the count', () => {
+    expect(wrapper.vm.count).toBe(0)
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.vm.count).toBe(1)
+  })
+})
+
+
+
+
+
+expect.extend({
+  toBeNumber(received, argument) {
+    const pass = Number.isInteger(received) === true;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} to be Integer`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected ${received} to not be Integer`,
+        pass: false
+      }
+    }
+  }
+})
+expect.extend({
+  toBeDivisibleBy(received, argument) {
+    const pass = received % argument == 0;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be divisible by ${argument}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected ${received} to be divisible by ${argument}`,
+        pass: false,
+      };
+    }
+  },
+
 })
